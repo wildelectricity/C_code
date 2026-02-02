d9c950e83f06d8ca5284ae5b1f19afa8a18e52fd
@heartless-c-language <1160474508@qq.com>
2026-02-02 10:41:30 +0800

    2026-2-2-作业

---
+++ test_1_16/demo.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	if (a >= b) //5 8
+//		printf("hehe\n");
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	if (a == b) //3 3
+		printf("hehe\n");
+	return 0;
+}
