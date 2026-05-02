956c4cf8eb8e9f44e26fb4d396af95228523c364
@heartless-c-language <1160474508@qq.com>
2026-05-02 22:52:25 +0800

    2026-5-2-作业

---
+++ test_5_20/test.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("hello world");
+//	printf("hello world\n");
+//	abc
+//	def
+//	printf("abc\ndef");
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	printf("%dabc\n", 200);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf(" % s will come tonight\n", "lisi");
+//	printf(" % s will come tonight\n", "wangwu");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%s says it is %d o'clovk\n","lisi", 10);
+//	printf("%s says it is %d o'clovk\n", "cuihua", 24);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%hd\n", 100);
+//	printf("%ho\n", 100);
+//	printf("%x\n", 15);//0 1 2 3 4 5 6 7 8 9 a b c d e f
+//	return 0;
+//}
+
+int main()
+{
+	printf("%d\n",123);
+	printf("%5d\n",123);
+	printf("%5d\n", 123456);//%5d×îÐ¡¿í¶È
+
+	return 0;
+}
+
